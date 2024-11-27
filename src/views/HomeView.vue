e8b09b4a7ae30a97c16fa79c79a8ba046fb320d5
Atrofos-kaliev <kalievv01a@gmail.com>
2024-11-27 10:33:11 +0500

    feat: add layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,137 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
 </script>
-
 <template>
-<h1> hi </h1>
-</template>
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col md-5">
+        <select class="form-select" aria-label="Default select example">
+    <option selected>Select category</option>
+    <option value="1">Vegetables</option>
+    <option value="2">Fruits</option>
+    <option value="3">Berries</option>
+  </select>
+  </div>
+        <div class="col md-5">
+          <input type="text" class="form-control" value="Search by name" aria-label="readonly input example">
+        </div>
+          <div class="col md-2">
+            <button type="button" class="btn btn-outline-warning">warning</button>
+      </div>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card">
+        <img src="https://greenspoon.co.ke/wp-content/uploads/2021/11/Greenspoon-Kwik-Basket-Sweet-Banana.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Banana</h5>
+          <p class="card-text">15$</p>
+          <p class="card-text">Fruit</p>
+          <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="https://www.finedininglovers.com/sites/g/files/xknfdk626/files/styles/article_1200_800_fallback/public/2021-10/best-apples%C2%A9iStock.jpg?itok=xvBiRvy1" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Apple</h5>
+          <p class="card-text">10$</p>
+          <p class="card-text">Fruit</p>
+          <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="https://www.grozny-inform.ru/LoadedImages/2024/01/20/1677474656_3-26_w900_h600.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Passion fruit</h5>
+          <p class="card-text">25$</p>
+          <p class="card-text">Fruit</p>
+          <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="https://i.pinimg.com/originals/bd/96/09/bd9609418c69c6edf65c2c351dd4f57b.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Kiwi</h5>
+          <p class="card-text">17$</p>
+          <p class="card-text">Fruit</p>
+          <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="https://avatars.mds.yandex.net/i?id=3bc4c225a5c2881bfce95815f0f90639_l-5916170-images-thumbs&n=13" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Water Melon</h5>
+          <p class="card-text">7$</p>
+          <p class="card-text">Berry</p>
+          <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="https://posyltorg33.ru/upload/iblock/bdc/bdc6765987ae2f66379fa6d7451915e1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Potato</h5>
+          <p class="card-text">5$</p>
+          <p class="card-text">Vegetables</p>
+          <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="https://ogodacha.ru/images/big-images/plodovye-derevya-i-kusty/golybika/obrabotka-golubiki-ot-boleznej-vesnoj/golubika-na-vetke.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Blueberry</h5>
+          <p class="card-text">12$</p>
+          <p class="card-text">Fruit</p>
+          <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="https://static.mk.ru/upload/entities/2022/02/24/16/articles/facebookPicture/3b/90/95/33/af197559768ef0e41bc41438b86682dc.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Cabbage</h5>
+          <p class="card-text">7$</p>
+          <p class="card-text">Vegetables</p>
+          <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+  </div>
+  </template>
